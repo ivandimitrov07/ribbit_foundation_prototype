d466709f6e85402fadebb7b12cebfe4433eca7f0
ivandimitrov07 <idimitrov07@yahoo.com>
2013-12-17 17:11:40 +0200

    initial commit-ribbit_foundation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RibbitFoundationPrototype::Application.config.secret_token = 'c42bdc27c934bbf0cc5e38dbe4ee554348461c14b337567008ad4cc7c891afa467b4f4faf1bac1839d6a7c70fb15115a57577e5e08511e634065b68ab2203c8f'
